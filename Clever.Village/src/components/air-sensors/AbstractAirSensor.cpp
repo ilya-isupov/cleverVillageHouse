9d1427fd1c7d809df132ba4d3ee449c69520d046
Ant334onio <tony.isupov2010@yandex.ru>
2005-12-31 23:09:14 +0300

    fixed bags

--- Clever.Village/src/components/air-sensors/AbstractAirSensor.cpp
+++ Clever.Village/src/components/air-sensors/AbstractAirSensor.cpp
@@ -1,10 +1,3 @@
 
-
 #include "AbstractAirSensor.h"
 
-class AbstractAirSensor : public SimpleHardwareUnit {
-    public:
-        AbstractAirSensor() : SimpleHardwareUnit() {};
-        AbstractAirSensor(unsigned int pin) : SimpleHardwareUnit(pin, INPUT) {};
-        ~AbstractAirSensor() {};
-};
