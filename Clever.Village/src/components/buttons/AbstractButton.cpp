b760cea42c1b81b514205ea4152a55c5136612ce
Ilya Isupov <ilia.isupov@yandex.ru>
2018-08-22 18:39:51 +0300

    Add AbstractButton implementation

--- Clever.Village/src/components/buttons/AbstractButton.cpp
+++ Clever.Village/src/components/buttons/AbstractButton.cpp
@@ -3,12 +3,15 @@
 
 #include "../common/HardwareUnit.cpp"
 
-class AbstractButton : public HardwareUnit {
-    public:
-        AbstractButton() : HardwareUnit() {};
-        AbstractButton(unsigned int pin, int mode) : HardwareUnit(pin, mode) {};
-        ~AbstractButton() {};
-              
+class AbstractButton : public HardwareUnit
+{
+  public:
+    AbstractButton() : HardwareUnit(){};
+    AbstractButton(unsigned int pin, int mode) : HardwareUnit(pin, mode){};
+    ~AbstractButton(){};
+    inline virtual void onClick(){};
+    inline virtual void onHold(){};
+    void run();
 };
 
 #endif
