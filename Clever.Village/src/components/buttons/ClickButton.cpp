976359478b0e44388c066a764d4e6721cb394bf9
Ant334onio <tony.isupov2010@yandex.ru>
2018-08-22 13:17:45 +0400

    Add OneClickButton and HoldButton

--- Clever.Village/src/components/buttons/ClickButton.cpp
+++ Clever.Village/src/components/buttons/ClickButton.cpp
@@ -4,12 +4,52 @@
 #include "AbstractButton.cpp"
 
 class ClickButton : AbstractButton {
+
+	unsigned int pin = 0;
+	
+	boolean butt_flag;
+	boolean butt;
+	boolean led_flag;
+	unsigned long last_press;
+	
     public:
-        ClickButton() {};
+        ClickButton() {
+			
+		};
         ClickButton(unsigned int pin, int mode) {
             AbstractButton(pin, mode);
+			pin = pin.this;
         };
         ~ClickButton();
+		
+		boolean ClickButtonOnce() {
+			boolean result;
+			butt = !digitalRead(pin);
+	if (butt == 1 && butt_flag == 0 && millis() - last_press > 50) { 
+    butt_flag = 1;
+   // Serial.println("Button pressed");
+    last_press = millis();
+	result = true;
+  }
+    if (butt == 0 && butt_flag == 1) {
+    butt_flag = 0;
+	result = false;
+  }
+			return result;
+		}
+		
+		boolean HoldButton() {
+			boolean result;
+			if (butt == 1 && butt_flag == 1 && millis() - last_press > 200) {
+			//Serial.println("Button hold");
+			result = true;
+    }		   
+	if (butt == 0 && butt_flag == 1) {
+    butt_flag = 0;
+	result = false;
+  }
+			return result;
+		}
 };
 
 #endif
