41cb093de17b5c520841cf188a63b14557ce2c2b
Ant334onio <tony.isupov2010@yandex.ru>
2018-08-22 23:49:29 +0400

    correct distructor

--- Clever.Village/src/components/buttons/AbstractButton.cpp
+++ Clever.Village/src/components/buttons/AbstractButton.cpp
@@ -2,7 +2,7 @@
 
     AbstractButton::AbstractButton() : HardwareUnit(){};
     AbstractButton::AbstractButton(unsigned int pin, int mode) : HardwareUnit(pin, mode){};
-    ~AbstractButton(){};
+    AbstractButton::~AbstractButton(){};
     inline void AbstractButton::onClick(){};
     inline void AbstractButton::onHold(){};
     void AbstractButton::run() {}
