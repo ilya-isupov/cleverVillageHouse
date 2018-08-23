d1a34492fa72e3322f33492dc35f8295109c4cc8
Ant334onio <tony.isupov2010@yandex.ru>
2018-08-23 21:36:44 +0400

    Add setup and run methods

--- Clever.Village/src/components/buttons/ClickButton.cpp
+++ Clever.Village/src/components/buttons/ClickButton.cpp
@@ -7,12 +7,46 @@
 	};
     ClickButton::~ClickButton() {};
 
+    void ClickButton::setup() {
+        pinMode(pin, mode);
+    }
+    void ClickButton::run() {
+      button1S = !digitalRead(pin);
+     // нажали (с антидребезгом)
+  if (button1S && !button1F && millis() - button1_timer > debounce) {
+    button1F = 1;
+    button1HO = 1;
+    button1_timer = millis();
+  }
+  // если отпустили до hold, считать отпущенной
+  if (!button1S && button1F && !button1R && !button1DP && millis() - button1_timer < hold) {
+    button1R = 1;
+    button1F = 0;
+    button1_double = millis();
+  }
+  // если отпустили и прошло больше double_timer, считать 1 нажатием
+  if (button1R && !button1DP && millis() - button1_double > double_timer) {
+    button1R = 0;
+    button1P = 1;
+  }
+
+  // Если удерживается более hold, то считать удержанием
+  if (button1F && !button1D && !button1H && millis() - button1_timer > hold) {
+    button1H = 1;
+  }
+  // Если отпущена после hold, то считать, что была удержана
+  if (!button1S && button1F && millis() - button1_timer > hold) {
+    button1F = 0;
+    button1H = 0;
+    button1_timer = millis();
+  }
+    }
     boolean ClickButton::ClickButtonOnce()
     {
-		//реализация нажатия кнопки
+		
     }
 
     boolean ClickButton::HoldButton()
     {
-        // реализация удержания кнопки
+        
     }
