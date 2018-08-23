2bb35eac411864336015d487cfd439a3610248a2
Ant334onio <tony.isupov2010@yandex.ru>
2018-08-23 23:18:48 +0400

    fixed bags

--- Clever.Village/src/components/buttons/ClickButton.cpp
+++ Clever.Village/src/components/buttons/ClickButton.cpp
@@ -1,62 +1,69 @@
 #include "ClickButton.h"
 
     ClickButton::ClickButton() : AbstractButton() {};
-    ClickButton::ClickButton(unsigned int pint, int modet) : AbstractButton(pin, mode){
-		pin = pint;
-		mode = modet;
+    ClickButton::ClickButton(unsigned int pin, int mode) : AbstractButton(pin, mode){
+        //
 	};
     ClickButton::~ClickButton() {};
 
     void ClickButton::setup() {
         pinMode(pin, mode);
     }
-    void ClickButton::run() {
-      button1S = !digitalRead(pin);
-     // нажали (с антидребезгом)
-  if (button1S && !button1F && millis() - button1_timer > debounce) {
-    button1F = 1;
+    void ClickButton::buttonDeBounce() {
+  if (button1S && !button1Flag && millis() - button1timer > debounce) {
+    button1Flag = 1;
     button1HO = 1;
-    button1_timer = millis();
-  }
-  // если отпустили до hold, считать отпущенной
-  if (!button1S && button1F && !button1R && !button1DP && millis() - button1_timer < hold) {
-    button1R = 1;
-    button1F = 0;
-    button1_double = millis();
-  }
-  // если отпустили и прошло больше double_timer, считать 1 нажатием
-  if (button1R && !button1DP && millis() - button1_double > double_timer) {
-    button1R = 0;
-    button1P = 1;
-  }
-
-  // Если удерживается более hold, то считать удержанием
-  if (button1F && !button1D && !button1H && millis() - button1_timer > hold) {
-    button1H = 1;
-  }
-  // Если отпущена после hold, то считать, что была удержана
-  if (!button1S && button1F && millis() - button1_timer > hold) {
-    button1F = 0;
-    button1H = 0;
-    button1_timer = millis();
+    button1timer = millis();
+      }
+    }
+    void ClickButton::buttonBeforeHold() {
+      if (!button1S && button1Flag && !button1Release && !button1DP && millis() - button1timer < hold) {
+        button1Release = 1;
+        button1Flag = 0;
+        button1double = millis();
+      }
+    }
+    void ClickButton::buttonAfterDoubleTimer() {
+       if (button1Release && !button1DP && millis() - button1double > doubletimer) {
+        button1Release = 0;
+        button1Press = 1;
   }
     }
-    boolean ClickButton::ClickButtonOnce()
+    void ClickButton::buttonAfterHold() {
+        // Если удерживается более hold, то считать удержанием
+  if (button1Flag && !buttonDouble && !button1Hold && millis() - button1timer > hold) {
+    button1Hold = 1;
+  }
+  // Если отпущена после hold, то считать, что была удержана
+  if (!button1S && button1Flag && millis() - button1timer > hold) {
+    button1Flag = 0;
+    button1Hold = 0;
+    button1timer = millis();
+  }
+    }
+    void ClickButton::run() {
+      button1S = !digitalRead(pin);
+     ClickButton::buttonDeBounce(); // нажали (с антидребезгом)
+     ClickButton::buttonBeforeHold(); // если отпустили до hold, считать отпущенной
+     ClickButton::buttonAfterDoubleTimer();  // если отпустили и прошло больше double_timer, считать 1 нажатием
+     ClickButton::buttonAfterHold(); // после удержания
+    }
+    boolean ClickButton::isClickButtonOnce()
     {
 		boolean res;
-		if (button1P) {
-            button1P = 0;
+		if (button1Press) {
+            button1Press = 0;
             res = true;
         } else 
         res = false;
         return res;
     }
 
-    boolean ClickButton::HoldButton()
+    boolean ClickButton::isHoldButton()
     {
         boolean res;
-		if (button1H) {
-            button1H = 0;
+		if (button1Hold) {
+            button1Hold = 0;
             res = true;
         } else 
         res = false;
--- Clever.Village/src/components/buttons/ClickButton.h
+++ Clever.Village/src/components/buttons/ClickButton.h
@@ -9,20 +9,24 @@ class ClickButton : public AbstractButton
 	int mode;
     
     boolean button1S;   // храним состояния кнопок (S - State)
-    boolean button1F;   // флажки кнопок (F - Flag)
-    boolean button1R;   // флажки кнопок на отпускание (R - Release)
-    boolean button1P;   // флажки кнопок на нажатие (P - Press)
-    boolean button1H;   // флажки кнопок на удержание (многократный вызов) (H - Hold)
+    boolean button1Flag;   // флажки кнопок (F - Flag)
+    boolean button1Release;   // флажки кнопок на отпускание (R - Release)
+    boolean button1Press;   // флажки кнопок на нажатие (P - Press)
+    boolean button1Hold;   // флажки кнопок на удержание (многократный вызов) (H - Hold)
     boolean button1HO;  // флажки кнопок на удержание (один вызов при нажатии) (HO - Hold Once)
-    boolean button1D;   // флажки кнопок на двойное нажатие (D - Double)
+    boolean buttonDouble;   // флажки кнопок на двойное нажатие (D - Double)
     boolean button1DP;  // флажки кнопок на двойное нажатие и отпускание (D - Double Pressed)
 
-    #define double_timer 100   // время (мс), отведённое на второе нажатие
+    #define doubletimer 100   // время (мс), отведённое на второе нажатие
     #define hold 500           // время (мс), после которого кнопка считается зажатой
     #define debounce 80        // (мс), антидребезг
-    unsigned long button1_timer; // таймер последнего нажатия кнопки
-    unsigned long button1_double; // таймер двойного нажатия кнопки
+    unsigned long button1timer; // таймер последнего нажатия кнопки
+    unsigned long button1double; // таймер двойного нажатия кнопки
 
+    void buttonDeBounce();
+    void buttonBeforeHold();
+    void buttonAfterDoubleTimer();
+    void buttonAfterHold();
   public:
     ClickButton();
     ClickButton(unsigned int pint, int mode);
@@ -30,8 +34,8 @@ class ClickButton : public AbstractButton
     void setup();
     void run();
 
-    boolean ClickButtonOnce();
-    boolean HoldButton();
+    boolean isClickButtonOnce();
+    boolean isHoldButton();
 };
 
 #endif
