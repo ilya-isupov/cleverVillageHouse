cd6e5484f50a2ab624c0f1adbf0b13fa51890891
Ant334onio <tony.isupov2010@yandex.ru>
2018-08-28 00:14:31 +0400

    add new TemperatureSensor.cpp .h

--- Clever.Village/src/components/temperature-sensors/AirTemperatureHumiditySensor.h
+++ Clever.Village/src/components/temperature-sensors/AirTemperatureHumiditySensor.h
@@ -1,35 +1,17 @@
 #ifndef AirTemperatureHumiditySensor_H
 #define AirTemperatureHumiditySensor_H
 
-#if ARDUINO >= 100
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
-
 #include "AbstractTemperatureSensor.h"
-
-
-#define TemperatureSensor11 11
-#define TemperatureSensor22 22
-#define TemperatureSensor21 21
+#include  "lib/iarduino_AM2320.h"
 
 class AirTemperatureHumiditySensor : public AbstractTemperatureSensor{
   public:
-   AirTemperatureHumiditySensor(uint8_t type, uint8_t count=6);
-   int readTemperature(int PINS);
-   int readHumidity(int PONS);
-   boolean read(void);
+  void begin();
+  int getTemperature();
+  int getHumidity();
 
  private:
-  uint8_t data[6];
-  uint8_t pin, _type, bit, port;
-  uint32_t lastreadtime, maxcycles;
-  bool firstreading;
-  bool lastresult;
-
-  uint32_t expectPulse(bool level);
-
+  iarduino_AM2320 sensor;
 };
 
 #endif
