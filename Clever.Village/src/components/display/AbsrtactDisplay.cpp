88bedb652e3999203fba3d3e4d5b109772b8afec
Ant334onio <tony.isupov2010@yandex.ru>
2018-08-29 13:17:13 +0400

    Add AbstractDisplay.h .cpp and library

---
+++ Clever.Village/src/components/display/AbsrtactDisplay.cpp
@@ -0,0 +1,3 @@
+
+#include "AbsrtactDisplay.h"
+
---
+++ Clever.Village/src/components/display/AbsrtactDisplay.h
@@ -0,0 +1,12 @@
+#ifndef Abstract_Display_h
+#define Abstract_Display_h
+
+#include "../common/ExtendedHardwareUnit.h"
+
+class AbstractDisplay : public ExtendedHardwareUnit {
+    public:
+        AbstractDisplay();
+        ~AbstractDisplay();
+};
+
+#endif
