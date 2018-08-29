caf582ca8bba3baa0173eaefb3025937b03932d3
Ant334onio <tony.isupov2010@yandex.ru>
2018-08-29 14:32:27 +0400

    load DisplayI2C1602

--- Clever.Village/src/components/display/DisplayI2C1602.h
+++ Clever.Village/src/components/display/DisplayI2C1602.h
@@ -1,7 +1,17 @@
 #include "AbsrtactDisplay.h"
-#include <Display1602.h>
+#include <LiquidCrystal_1602_RUS.h>
 
 class DisplayI2C1602 : public AbstractDisplay {
-    DisplayI2C1602();
-    ~DisplayI2C1602();
-}
+    public:
+        DisplayI2C1602();
+        DisplayI2C1602(uint8_t rs,  uint8_t enable, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3);
+        ~DisplayI2C1602();
+        LiquidCrystal_1602_RUS getLCD();
+        void setup();
+        void setCursor();
+        void print();
+        void clear();
+
+    private:
+    LiquidCrystal_1602_RUS lcd();
+};
