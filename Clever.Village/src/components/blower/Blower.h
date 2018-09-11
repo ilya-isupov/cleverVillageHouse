e4ceb8451ff72854d22cb16ee383e3fc768a56f8
Ant334onio <tony.isupov2010@yandex.ru>
2018-09-11 11:58:17 +0400

    Add blower .h .cpp

---
+++ Clever.Village/src/components/blower/Blower.h
@@ -0,0 +1,18 @@
+
+#ifndef Blower_h
+#define Blower_h
+
+#include "../common/SimpleHardwareUnit.h"
+
+class Blower : public SimpleHardwareUnit {
+
+    public: 
+        Blower(unsigned int pin);
+        Blower();
+        ~Blower();
+        void setMode(bool value);
+    private:
+        unsigned int pin;
+
+};
+#endif
