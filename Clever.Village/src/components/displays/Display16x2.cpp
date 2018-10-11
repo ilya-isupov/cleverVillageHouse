f5f894febd077b3d99d1412d84368b9dc7ea7507
Ant334onio <tony.isupov2010@yandex.ru>
2018-10-11 20:29:26 +0400

    did Display.h and .cpp

--- Clever.Village/src/components/displays/Display16x2.cpp
+++ Clever.Village/src/components/displays/Display16x2.cpp
@@ -1,9 +1,33 @@
 #include "Display16x2.h"
 
-    Display16x2::Display16x2(uint8_t rs,  uint8_t enable, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3) {
-        
-    }
+Display16x2::Display16x2() {
+    this->lcd = new LiquidCrystal_I2C(0x42, 16, 2); 
+    this->displayMenu = new LiquidMenu(*lcd);
+}
 
-    LiquidMenu Display16x2::getLCD() {
-        return lcd();
-    }
+Display16x2::Display16x2(int type, uint8_t a, uint8_t b) {
+    this->lcd = new LiquidCrystal_I2C(type, a, b); 
+    this->displayMenu = new LiquidMenu(*lcd);
+}
+void Display16x2::setup() {
+    this->lcd->init();
+    this->lcd->backlight();
+    this->displayMenu->init();
+
+}
+void Display16x2::update() {
+    this->displayMenu->update();
+}
+void Display16x2::setFirstLine(byte column, byte row, String value) {
+    this->firstLine = new LiquidLine(column, row, value);
+}
+void Display16x2::setSecondLine(byte column, byte row, String value) {
+    this->secondLine = new LiquidLine(column, row, value);
+}
+void Display16x2::setScreen() {
+    this->screen = new LiquidScreen(*firstLine, *secondLine);
+    this->displayMenu->add_screen(*screen);
+}
+void Display16x2::nextScreen() {
+    this->displayMenu->next_screen();
+}
--- Clever.Village/src/components/displays/Display16x2.h
+++ Clever.Village/src/components/displays/Display16x2.h
@@ -1,17 +1,25 @@
 #include "AbsrtactDisplay.h"
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include <LiquidMenu.h>
 
 class Display16x2 : public AbstractDisplay {
     public:
         Display16x2();
         Display16x2(uint8_t rs,  uint8_t enable, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3);
+        Display16x2(int type, uint8_t a, uint8_t b);
         ~Display16x2();
-        LiquidMenu getLCD();
         void setup();
-        void setCursor();
-        void print();
-        void clear();
+        void setFirstLine(byte column, byte row, String value);
+        void setSecondLine(byte column, byte row, String value);
+        void setScreen();
+        void update();
+        void nextScreen();
 
     private:
-    LiquidMenu lcd();
+        LiquidCrystal_I2C *lcd;
+        LiquidMenu *displayMenu;
+        LiquidLine *firstLine;
+        LiquidLine *secondLine;
+        LiquidScreen *screen;
 };
